40b814dcfbf2b96f2836cd0e199fb280a5ca254d
Rafael <rpufal@itclinical.com>
2022-05-27 22:36:13 +0100

    initial setup/ missing radix

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head key="home">
+        <title>Homepage</title>
+      </Head>
+      <main >
+        <h1>Hellor world</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
